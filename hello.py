2fee134b2bec710a99e27cf980e61009d5d595a8
Nikita Busarov <nikitka.sarkisyan@mail.ru>
2025-11-23 19:19:26 +0300

    add: dirty hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#!/usr/bin/python2.7 -u
+#!/usr/bin/python
+# -*- coding: cp1251 -*-
+def main():
+    """
+    Говорит "Hello appsec world"
+    """
+    print("Hello appsec world")
+
+if __name__ == "__main__":
+    main()
